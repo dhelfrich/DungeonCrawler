e4e18b34291f1090a297f552c6236ca63ea236f9
elialdinger <49462755+elialdinger@users.noreply.github.com>
2021-11-16 09:45:46 -0700

    Add navigation button to new screens

--- app/src/main/java/com/example/dungeoncrawler/MainActivity.java
+++ app/src/main/java/com/example/dungeoncrawler/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.dungeoncrawler;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playButton = (Button) findViewById(R.id.playButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityGameplay();
+            }
+        });
+
+
+    }
+
+    public void openActivityGameplay() {
+        Intent intent = new Intent(this, GameplayActivity.class);
+        startActivity(intent);
     }
 }
